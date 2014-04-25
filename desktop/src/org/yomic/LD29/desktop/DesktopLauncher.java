f0fed208143ced7353b2229a67bcd25150e5f5bc
Yomic <trueyomic@yahoo.com>
2014-04-25 09:30:29 -0500

    Initial Commit
    First Commit before the compo to make sure everything is in order

---
+++ desktop/src/org/yomic/LD29/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.yomic.LD29.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.yomic.LD29.LD29;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LD29(), config);
+	}
+}
